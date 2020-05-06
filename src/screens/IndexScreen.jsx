ca2141034d3263fe3e74484aa52c2185f3cdae9a
arodriguuij <arodriguuij@gmail.com>
2020-05-06 16:21:26 +0200

    Creating Context to share state within the app, and button to add multiples posts

---
+++ src/screens/IndexScreen.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const { data, add } = useContext(BlogContext);
+  return (
+    <View>
+      <Text>Index scree</Text>
+      <Button title="Add Post" onPress={add}/>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
